a16e72dc122539c672f935521a964216e5b25bcd
Istvan Rozsa <pisti.rzsa@gmail.com>
2022-02-02 11:51:20 +0100

    add timezone config

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
+from dateutil import tz
 
 PORT = 0
 HOST = ""
@@ -20,6 +21,8 @@ with open(os.path.expanduser('~/Projects/nba-tracker/config.json')) as json_file
     EMAIL_ADDRESS = data["email"]["email_address"]
     PASSWORD = data["email"]["password"]
     API_KEY = data["apikey"]
+    MY_TIME_ZONE = data["timezone"]
+    
     
 
 
@@ -41,10 +44,19 @@ with open(os.path.expanduser('~/Projects/nba-tracker/datas/today_matches.json'),
 
 
 
+def toMyTimeZone(date):
+    from_zone = tz.gettz('UTC')
+    to_zone = tz.gettz(MY_TIME_ZONE) #'Europe/Debrecen'
+    utc = datetime.strptime(date, "%Y-%m-%dT%H:%M:%S+00:00") #"2022-02-02T00:00:00+00:00"
+    utc = utc.replace(tzinfo=from_zone)
+    my_date_time = utc.astimezone(to_zone)
+    return my_date_time
+    
+    
 class Match:
-    def __init__(self, date, time, home, away):
-        self.date =date
-        self.time = time
+    def __init__(self, date, home, away):
+        self.date = toMyTimeZone(date).strftime("%Y-%m-%d")
+        self.time = toMyTimeZone(date).strftime("%H:%M")
         self.home = home
         self.away = away
         
@@ -62,6 +74,7 @@ class Match:
                 <li>Kezdés: {time}</li>
             </ul>
             """.format(home = self.home, away = self.away, time = self.time)
+
         
 matches_html = ""
 with open(os.path.expanduser('~/Projects/nba-tracker/datas/today_matches.json')) as json_file:
@@ -69,7 +82,7 @@ with open(os.path.expanduser('~/Projects/nba-tracker/datas/today_matches.json'))
     
     for match in data['response']:
         if match['league']['id']==12:
-            nba_match = Match(match['date'],match['time'],match['teams']['home']['name'], match['teams']['away']['name'])
+            nba_match = Match(match['date'],match['teams']['home']['name'], match['teams']['away']['name'])
             matches_html += nba_match.toHTML()
             print(nba_match.toJSON())
 
